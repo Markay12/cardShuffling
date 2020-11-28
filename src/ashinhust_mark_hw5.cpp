cf69a326b57ddd0d4c1ddd7bc573b6bb50bd4252
Mark Ashinhust <ashinhust.brass@gmail.com>
2020-11-27 18:17:54 -0700

    main program file

--- src/ashinhust_mark_hw5.cpp
+++ src/ashinhust_mark_hw5.cpp
@@ -18,32 +18,16 @@ using std::cout;
 int main()
 {
 
-	
-	Cards* deck1 = new Cards();
 
-	cout << "\nYour original card value is --> " << deck1->getCardVal() << " of " << deck1->getCardSuit() << "\n";	
+	BagNode* deck1 = new BagNode();
+	deck1->addRandom();
 
-	deck1->setCard();
+	free(deck1);
 
-	cout << "\nYour new card value is --> " << deck1->getCardVal() << " of " << deck1->getCardSuit() << "\n";
+	cout << "\nThe first value is --> " << deck1->getCurrVal() << " of " << deck1->getCurrSuit() << "\n";
 
-	BagNode deck2;
 
-	deck2.dealCard();
-
-	cout << "\n---------------\nCreated a new Deck of " << deck2.getCurrentSize() << " cards\n";	
-
-	deck2.empty();	
-	cout << "\nThe deck has been emptied and now has " << deck2.getCurrentSize() << " cards\n";
-	deck2.fill();
-
-	cout << "\nCreating a new deck of cards\n\nNew deck created with " << deck2.getCurrentSize() << " cards\n";
-
-	cout << "The value of card 1 in the deck is " << deck2.getCurrVal() << "\nand the suit is " << deck2.getCurrSuit() << "\n\n";
-	
 	return 0;
 
 }
-
-
 
