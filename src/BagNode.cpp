100aa78ce75ad3045c302c75458a618235139a06
Mark Ashinhust <ashinhust.brass@gmail.com>
2020-11-27 18:05:26 -0700

    c++ file with written methods for linkedlist

--- src/BagNode.cpp
+++ src/BagNode.cpp
@@ -4,37 +4,40 @@
 
 #include "BagNode.h"
 
+using std::cout;
+
 //default constructor
 BagNode::BagNode()
 {
-	
-	int total_face = 13;
-	int total_suits = 4;
-
-	for (int i = 0; i < total_suits; i++)
-	{
-		for(int j = 0; j < total_face; j++)
-		{
-
-			Cards newCard;
-			this->bag.push_back(newCard);
-		}
-	}
-	this->currCard = 0; //begin at first pointer value	
 
 }
 
 char BagNode::getCurrVal()
 {
 
-	return bag[currCard].getCardVal();
+	if (first == NULL)
+	{
+		return NULL;
+	}
+	else
+	{
+		return first->currentCard->getCardVal();
+	}	
 
 }
 
 char BagNode::getCurrSuit()
 {
 
-	return bag[currCard].getCardSuit();
+	if (first == NULL)
+	{
+		return NULL;
+	}
+	else
+	{
+		return first->currentCard->getCardSuit();
+	}
+
 }
 
 
@@ -42,7 +45,7 @@ void BagNode::empty()
 {
 
 	//clear all elements in our bag vector
-	bag.clear();
+	
 
 }
 
@@ -50,23 +53,7 @@ void BagNode::fill()
 {
 
 	//same method as initialize but we create a whole new stack of cards
-	bag.clear();
-
-	int total_face = 13;
-	int total_suits = 4;
-
-	for(int i = 0; i < total_suits; i++)
-	{
-		for (int j = 0; j < total_face; j++)
-		{
-
-			Cards randomCard;
-			this->bag.push_back(randomCard);
-		}
-	}
-	this->currCard = 0; //begin at first pointer value
 }
-
 
 //return current card in deck and increment to next
 //
@@ -74,23 +61,82 @@ void BagNode::fill()
 Cards BagNode::dealCard()
 {
 
-	if (!this->isEmpty()) //while our deck isn't empty
-	{
-		int nextCard = this->currCard++;
-		return this->bag[nextCard];
-	}
-
-	//Yes, I know we're going to have issues here, fix this later. Shouldn't be an issue
-	//right now	
 }
 
 bool BagNode::isEmpty()
 {
-	return this->currCard < (int)this->bag.size();
 }
 
 int BagNode::getCurrentSize()
 {
-
-	return (int)this->bag.size();
 }
+
+
+void BagNode::display(struct BagNode* node)
+{
+
+
+}
+
+void BagNode::addCard(Cards* targetCard)
+{
+
+	Cards* newCard = new Cards();
+	newCard->setCard(); //give our new card set suit and value
+
+	//creation of new node
+	nodePointer addNode = new node;
+
+	addNode->next = NULL;
+	addNode->currentCard = newCard; //copy new Card constructor
+
+	if (first != NULL)
+	{
+		current = first;
+
+		//check if we are at the end of the linked list
+		while(current->next != NULL)
+		{
+			current = current->next;
+
+		}
+
+		//last node relation to the new ndoe
+		current->next = addNode;
+	}
+	else
+	{
+		//new node becomes the front of the list if linked list is empty
+		first = addNode;
+	}
+
+}
+
+void BagNode::addRandom()
+{
+
+	Cards* newCard = new Cards(); //instantiate new object
+	newCard->setCard(); //set the cards value
+
+	nodePointer addNode = new node;
+
+	addNode->next = NULL;
+	addNode->currentCard = newCard; //copy this new card constructor
+
+	if (first != NULL)
+	{
+		current = first;
+
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+
+		current->next = addNode;
+	}
+	else
+	{
+		first = addNode;
+	}
+}
+
